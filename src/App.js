388c6625edab4edde237c24b695950c7ef4a2d17
Imran <imran1510043@gmail.com>
2022-04-14 13:54:02 +0600

    html and javascript portion done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./components/styles.css"
+import info from './components/data';
+//import Loading from './components/Loading';
+import Tours from './components/Tours';
 
 const App = () => {
+  //const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState(info);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+  // if(loading) {
+  //   return (
+  //     <main>
+  //       <Loading />
+  //     </main>
+  //   )
+  // }
+
+  if(tours.length === 0) {
+    return (
+      <main>
+        <div>
+          <h2>no tours left</h2>
+          <button onClick={() => setTours(info)}>refresh</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
-    <div className="">
-      <h1>Our Tours</h1>
-    </div>
-  );
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
 }
 
 export default App;
